067c5668f1d825aa95ada56ad5d3ff7cb9b6ab23
Maik Switalski <Maik.Switalski@gmx.de>
2015-06-15 13:16:12 +0200

    added Desktop support.

---
+++ Desktop/MockedSimpleTimer.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace PerpetualEngine
+{
+    public partial class SimpleTimer
+    {
+        static SimpleTimer()
+        {
+            SimpleTimer.Create = () => {
+                return new MockedSimpleTimer();
+            };
+        }
+    }
+
+    class MockedSimpleTimer : SimpleTimer
+    {
+        bool running = false;
+
+        public override void Repeat(TimeSpan timeSpan, Action action, bool immediate = false)
+        {
+            if (immediate)
+                action();
+            RepeatAsync(timeSpan, action);
+        }
+
+        async void RepeatAsync(TimeSpan timeSpan, Action action)
+        {
+            running = true;
+            while (running) {
+                await timeSpan;
+                if (running) {
+                    action();
+                }
+            }
+        }
+
+        public override void Clear()
+        {
+            running = false;
+        }
+
+        public override bool IsRunning()
+        {
+            return running;
+        }
+    }
+}
+
---
+++ Shared/TimeSpanAwaiter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace PerpetualEngine
+{
+    public static class TimeSpanAwaiter
+    {
+        public static TaskAwaiter GetAwaiter(this TimeSpan timeSpan)
+        { 
+            return Task.Delay(timeSpan).GetAwaiter(); 
+        }
+    }
+}
+
